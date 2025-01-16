29c13cab594c7332c7177b9f2b819b69efb2b60e
Skaurova <skaurova34@gmail.com>
2025-01-16 15:58:24 +0900

    upd

---
+++ spam-checker.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import re
+import string
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+df = pd.read_csv("emails.csv")
